184637c74bc874bef9f43706fde8e504c68d41e4
ciricode <ciricode@protonmail.com>
2022-07-15 17:25:40 +0700

    improve UI

--- assets/tiktok.jsx
+++ assets/tiktok.jsx
@@ -42,38 +42,51 @@ function App() {
         <i className="fa-brands fa-tiktok" />
       </div>
       {notification && <div className={`notification ${error ? 'is-danger' : 'is-success'}`}>{notification}</div>}
-      <div className="box">
-        <div className="field">
-          <label htmlFor="input" className="label">
-            URL
+      <div className="card">
+        <div className="card-header">
+          <button type="button" className="card-header-icon" aria-label="more options">
+            <span className="icon">
+              <i className="fa-solid fa-circle" style={{ color: '#ff5f56' }} />
+            </span>
+            <span className="icon">
+              <i className="fa-solid fa-circle" style={{ color: '#ffbd2e' }} />
+            </span>
+            <span className="icon">
+              <i className="fa-solid fa-circle" style={{ color: '#27c93f' }} />
+            </span>
+          </button>
+          <p className="card-header-title">Get link TikTok</p>
+        </div>
+        <div className="card-content">
+          <div className="field">
             <div className="control has-icons-left">
               <input id="input" className="input" type="text" value={input} onChange={(e) => setInput(e.target.value)} />
               <span className="icon is-small is-left">
                 <i className="fa-solid fa-link" />
               </span>
             </div>
-          </label>
-        </div>
-        <div className="field">
-          <div className="control">
-            <label htmlFor="watermark" className="checkbox">
-              <input id="watermark" type="checkbox" defaultChecked={watermark} onChange={() => setWatermark(!watermark)} />
-              {' Remove watermark'}
-            </label>
           </div>
-        </div>
-        <div className="field">
-          <div className="control">
-            <button
-              type="submit"
-              className={`button is-primary${isLoading ? ' is-loading' : ''}`}
-              onClick={() => handleChange()}
-            >
-              <span className="icon is-small">
-                <i className="fa-solid fa-floppy-disk" />
-              </span>
-              <span>Download</span>
-            </button>
+          <div className="field">
+            <div className="control">
+              <label htmlFor="watermark" className="checkbox">
+                <input id="watermark" type="checkbox" defaultChecked={watermark} onChange={() => setWatermark(!watermark)} />
+                {' Remove watermark'}
+              </label>
+            </div>
+          </div>
+          <div className="field">
+            <div className="control">
+              <button
+                type="submit"
+                className={`button is-success is-outlined${isLoading ? ' is-loading' : ''}`}
+                onClick={() => handleChange()}
+              >
+                <span className="icon is-small">
+                  <i className="fa-solid fa-floppy-disk" />
+                </span>
+                <span>Download</span>
+              </button>
+            </div>
           </div>
         </div>
       </div>
